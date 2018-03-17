372f6231f765b47c3c4b06f6fc09485af93e1e89
litwachi <katuka.wilfred8@gmail.com>
2018-03-17 11:33:33 +0300

    hello.py

---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' :'Article one',
+            'body': 'Lorem If you want to get a copy of an existing Git repository — for example, a project you’d like to contribute to — the command you need is git clone.',
+            'author':'Wilfred Katuka',
+            'create_date':'04-25-2017',
+
+
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem If you want to get a copy of an existing Git repository — for example, a project you’d like to contribute to — the command you need is git clone.',
+            'author': 'Karsten Katuka',
+            'create_date': '04-25-2017',
+
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem If you want to get a copy of an existing Git repository — for example, a project you’d like to contribute to — the command you need is git clone.',
+            'author': 'Torrence Nalisi',
+            'create_date': '04-25-2017'
+
+        }
+    ]
+    return articles
