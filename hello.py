3311784011ac67aa80e5af0c4b9aca1f16f6e8d6
Wilfred Katuka <katuka.wilfred8@gmail.com>
2018-05-25 22:29:20 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,7 +10,7 @@ app.debug = True
 #config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'l!tw@chi1990'
+app.config['MYSQL_PASSWORD'] = '******'
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 #init MYSQL
